27728572be164b2538d0b4272e8beccd19bdeef1
Andres Villamil <bcl.evillamil@gmail.com>
2026-01-20 20:20:39 -0500

    Add game catalog view and template for displaying game list

--- tienda_videojuegos/catalogo/views.py
+++ tienda_videojuegos/catalogo/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+def lista_juegos(request):
+    juegos = [
+        {'nombre': 'The Legend of Zelda: Breath of the Wild', 'precio': 59.99, 'plataforma': 'Nintendo Switch'},
+        {'nombre': 'God of War', 'precio': 39.99, 'plataforma': 'PlayStation 4'},
+        {'nombre': 'Halo Infinite', 'precio': 49.99, 'plataforma': 'Xbox Series X'},
+        {'nombre': 'Cyberpunk 2077', 'precio': 29.99, 'plataforma': 'PC'},
+        {'nombre': 'Minecraft', 'precio': 26.95, 'plataforma': 'Multiplataforma'},
+        {'nombre': 'Among Us', 'precio': 4.99, 'plataforma': 'Multiplataforma'},
+        {'nombre': 'Red Dead Redemption 2', 'precio': 59.99, 'plataforma': 'PlayStation 4'},
+        {'nombre': 'The Witcher 3: Wild Hunt', 'precio': 39.99, 'plataforma': 'PC'}
+    ]
+    contexto_catalogo_juegos = {
+        'lista_juegos': juegos
+    }
+    
+    return render(request, 'catalogo/lista_juegos.html', contexto_catalogo_juegos)
