c4619c83df2b7ebd393797278a653075e661548e
Andres Villamil <bcl.evillamil@gmail.com>
2026-01-21 08:29:28 -0500

    Add Juego model and migration for game catalog

--- tienda_videojuegos/tienda_videojuegos/settings.py
+++ tienda_videojuegos/tienda_videojuegos/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pc+grt=n0sg7h*tz2z)-=$c=ves^-09otnx3&gkw5)pl0pu9wt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost','partners-episode-years-spouse.trycloudflare.com']
 
 
 # Application definition
